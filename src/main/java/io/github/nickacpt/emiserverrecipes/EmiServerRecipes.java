e62100068bb11be2e498e9f4d17bf85baf3bb079
NickAc <32451103+NickAcPT@users.noreply.github.com>
2023-05-24 11:36:40 +0100

    Initial commit

---
+++ src/main/java/io/github/nickacpt/emiserverrecipes/EmiServerRecipes.java
@@ -0,0 +1,14 @@
+package io.github.nickacpt.emiserverrecipes;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class EmiServerRecipes implements ModInitializer {
+    public static final Logger LOGGER = LoggerFactory.getLogger("emi-server-recipes");
+
+    @Override
+    public void onInitialize() {
+    }
+}
